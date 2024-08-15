4c3adc12d7b1b76c7c5ee1c03a22584f08c6ac54
Amal Amsis <amalamsis19@gmail.com>
2024-08-15 13:25:31 +0300

    basic interfaces

---
+++ Engine/src/sheet/Sheet.java
@@ -0,0 +1,10 @@
+package sheet;
+
+import sheet.cell.Cell;
+
+public interface Sheet {
+    int getVersion();
+    String getTitle();
+    Cell getCell(int row, int column);
+    void setCell(int row, int column, String originalValue);g
+}
