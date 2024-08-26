01518dc9c6f7eb45c22f0b2367242529433fd4de
Yarden Daniel <yardend405@gmail.com>
2024-08-26 12:40:09 +0300

    ui module - first naive interface

---
+++ UI/src/UIManager.java
@@ -0,0 +1,14 @@
+public interface UIManager {
+
+    //Amal
+    void printMenu();
+    //yarden
+    void ptintSheet();
+    //yarden
+    void ptintCell();
+    //yarden
+    void loadXmlFile();
+    void loadSeralizationFile();
+    void saveSeralizationFile();
+
+}
