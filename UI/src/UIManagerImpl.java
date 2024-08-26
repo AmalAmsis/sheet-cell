b9158bb99dda577b143303e878020eec7401796e
Yarden Daniel <yardend405@gmail.com>
2024-08-26 18:54:28 +0300

    displaySheet func impl in the UI

---
+++ UI/src/UIManagerImpl.java
@@ -0,0 +1,95 @@
+import dto.DTOCell;
+import dto.DTOSheet;
+import engine.Engine;
+import engine.EngineImpl;
+
+import java.util.Map;
+
+public class UIManagerImpl {
+
+    public void displaySheet(){
+        Engine engine = new EngineImpl();
+        DTOSheet dtoSheet = engine.displaySheet();
+        printSheetToConsole(dtoSheet);
+    }
+
+    private void printSheetToConsole(DTOSheet dtoSheet) {
+        StringBuilder sb = new StringBuilder();
+
+        //Adding the title and the version of the sheet to the StringBuilder
+        printSheetHeader(dtoSheet, sb);
+
+        int numOfColumns = dtoSheet.getNumOfColumns();
+        int numOfRows = dtoSheet.getNumOfRows();
+        int widthOfColumn = dtoSheet.getWidthOfColumns();
+
+        // Adding the top row that represents the columns to the StringBuilder
+        printColumnHeaders(numOfColumns, widthOfColumn, sb);
+
+        // Iterating over each row to print its content
+        Map<String, DTOCell> cells = dtoSheet.getCells();
+        for (int row = 1; row <= numOfRows; row++) {
+            printRow(row, numOfColumns, widthOfColumn, cells, sb);
+        }
+
+        // Printing the final output to the console
+        System.out.println(sb.toString());
+    }
+
+    // Adding the title and the version of the sheet to the StringBuilder
+    private void printSheetHeader(DTOSheet dtoSheet, StringBuilder sb) {
+        sb.append("Sheet Title: ").append(dtoSheet.getSheetTitle()).append("\n")
+                .append("Sheet Version: ").append(dtoSheet.getSheetVersion()).append("\n\n");
+    }
+
+    // Adding the column headers (A, B, C, etc.) to the StringBuilder
+    private void printColumnHeaders(int numOfColumns, int widthOfColumn, StringBuilder sb) {
+        sb.append("    |");  //Leaving a space of 4 characters for line numbers
+        for (int col = 0; col < numOfColumns; col++) {
+            char columnLetter = (char) ('A' + col);
+            int paddingBefore = (widthOfColumn - 1) / 2;
+            int paddingAfter = widthOfColumn - 1 - paddingBefore;
+            sb.append(" ".repeat(paddingBefore)).append(columnLetter).append(" ".repeat(paddingAfter));
+            if (col < numOfColumns - 1) {
+                sb.append("|");
+            }
+        }
+        sb.append("\n");
+    }
+    // Adding each row, including the row number and cell values to the StringBuilder
+    private void printRow(int row, int numOfColumns, int widthOfColumn, Map<String, DTOCell> cells, StringBuilder sb) {
+        sb.append(String.format("%4d", row)).append(" |"); //print the number of the row
+        for (int col = 0; col < numOfColumns; col++) {
+            //create the key of yhe cell
+            char columnLetter = (char) ('A' + col);
+            String cellKey = columnLetter + ":" + row;
+
+            // Get the DTOCell associated with the key
+            DTOCell currentCell = cells.get(cellKey);
+            String currentCellValue;
+            StringBuilder sbCellValue = new StringBuilder();
+
+            // If the cell exists
+            if (currentCell != null) {
+                // Get the effective value as a string
+                currentCellValue = currentCell.getEffectiveValue().toString();
+
+                if (currentCellValue.length() > widthOfColumn) {  // If the value is too long, cut it to fit the cell width
+                    currentCellValue = currentCellValue.substring(0, widthOfColumn);
+                }
+                int cellPadding = widthOfColumn - currentCellValue.length();
+                sbCellValue.append(currentCellValue).append(" ".repeat(cellPadding));
+
+            } else { // If the cell does not exist, fill with spaces
+                sbCellValue.append(" ".repeat(widthOfColumn));
+            }
+            // Add "|" between columns, except after the last column
+            if (col < numOfColumns - 1) {
+                sbCellValue.append("|");
+            }
+            sb.append(sbCellValue);
+        }
+        sb.append("\n");  // Move to the next line after each row
+    }
+
+}
