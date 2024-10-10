57c48b3c0fd85131ffdc59833e980b911ad55bdd
Amal Amsis <amalamsis19@gmail.com>
2024-10-10 23:49:13 +0300

    starting part 3!!! linking the tomcat and dividing it into new models and starting work on login

---
+++ Client/src/component/login/LoginController.java
@@ -0,0 +1,17 @@
+package component.login;
+
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+
+import java.io.IOException;
+
+public class LoginController {
+
+}
