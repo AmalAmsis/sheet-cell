485060075f781c9f5baa60f785991e17225414bd
Amal Amsis <amalamsis19@gmail.com>
2024-10-12 16:39:59 +0300

    Prepared FXML and controller structure for dashboard layout
    - Created `dashboardHeader.fxml` to define the layout for the header section of the dashboard.
    - Added `DashboardHeaderController.java` to handle the logic and interaction for the header section.
    - Organized the project structure under `subcomponents/header` for better modularization.
    - Added placeholders for subcomponents in the dashboard (availableSheets, commands, and Permissions).

---
+++ Client/src/component/dashboard/subcomponents/header/DashboardHeaderController.java
@@ -0,0 +1,19 @@
+package component.dashboard.subcomponents.header;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashboardHeaderController {
+
+    @FXML
+    private Button loadSheetFileButton;
+
+    @FXML
+    void handleLoadSheetFile(ActionEvent event) {
+
+    }
+
+}
+
+
