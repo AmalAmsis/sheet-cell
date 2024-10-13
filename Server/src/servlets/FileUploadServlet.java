6d6d0eb83e77f9a95346e1c8429d587def3fad87
Yarden Daniel <yardend405@gmail.com>
2024-10-13 12:18:43 +0300

    commit for marge

--- Server/src/servlets/FileUploadServlet.java
+++ Server/src/servlets/FileUploadServlet.java
@@ -17,7 +17,7 @@ import java.io.InputStream;
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5)
 public class FileUploadServlet extends HttpServlet {
 
-    @Override public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         AllSheetsManager sheetsManager = ServletUtils.getSheetManager(getServletContext());
 
@@ -41,6 +41,10 @@ public class FileUploadServlet extends HttpServlet {
         {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
+    }
+
+    @Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("bla bla bla");
     }
 
 }
