9ad25b713ebdf169792fc87ffc1c88567d37de56
Amal Amsis <amalamsis19@gmail.com>
2024-10-14 14:52:44 +0300

    add cellServlet to Server

---
+++ Server/src/servlets/CellServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CellServlet extends HttpServlet {
+
+    //מידע על תא קיים
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+
+
+    }
+
+    //עדכון תא
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
