b2647fa19d0b14a686f621d5e1d0e235f72bca66
Yarden Daniel <yardend405@gmail.com>
2024-10-19 18:47:22 +0300

    Version Changes Servlet was established

---
+++ Server/src/servlets/VersionChangesServlet.java
@@ -0,0 +1,12 @@
+package servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "Version Changes Servlet", urlPatterns = {"/version-changes"})
+public class VersionChangesServlet extends HttpServlet {
+
+
+
+
+}
